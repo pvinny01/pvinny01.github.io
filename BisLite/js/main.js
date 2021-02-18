162bdfb3d7f4ad1e26b724e6eeeb465b1136484f
pvinny01 <pvinny01@github.com>
2021-02-18 09:45:45 +0300

    update

--- BisLite/js/main.js
+++ BisLite/js/main.js
@@ -8,6 +8,27 @@ $(function(){
     slidesToScroll: 1,
     prevArrow:'<button type="button" class="slick-prev slick-arrow"><img src="img/slider/arrow-left.svg" alt=""></button>',
     nextArrow:'<button type="button" class="slick-next slick-arrow"><img src="img/slider/arrow-right.svg" alt=""></button>',
+    responsive: [
+    {
+      breakpoint: 911,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 681,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 455,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+    
+      ]
   });
   $('.nav_btn').on('click', function(){
     $('.nav ul').slideToggle(); 
